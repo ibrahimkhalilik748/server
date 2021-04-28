957d172eded844c48906c63db3f8a1f5d20e6f58
Ibrahim Khalil <ibrahimkhalilik748@gmail.com>
2021-04-28 19:12:44 +0600

    hi

--- index.js
+++ index.js
@@ -23,38 +23,25 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://login-user:log1234@cluster0.em86h.mongodb.net/phishing?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productCollection = client.db("phishing").collection("logIn");
+  const userCollection = client.db("phishing").collection("logIn");
 
-  // app.get("/product", (req, res) => {
-  //   productCollection.find()
-  //   .toArray((err, product) => {
-  //     res.send(product)
-  //     console.log('from database', product)
-  //   })
-  // })
+  
 
   app.post('/addUser', (req, res) => {
     const logIn = req.body;
     console.log('adding new Product:', logIn)
-
-    productCollection.insertOne(logIn)
+    userCollection.insertOne(logIn)
       .then(result => {
         console.log('user', result.insertedCount);
         res.send(result.insertedCount > 0)
       })
   })
+  app.get('logIn', (req, res) => {
+    userCollection.find({}).toArray((err, result) => {
+      res.send(result)
+    })
+  })
 
-  // app.delete('/deleteProduct/:id', (req, res) => {
-
-    // productCollection.findOneAndDelete({_id: ObjectID(req.params.id)})
-    // .then( result => {
-    //   console.log(result)
-  //   // })
-
-  //   const id = ObjectID(req.params.id);
-  //   productCollection.findOneAndDelete({_id:id})
-  //   .then(document => res.send(!!document.value))
-  // })
 
   // client.close();
 });
